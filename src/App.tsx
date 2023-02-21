342c6c9598de1b25b70445e8263b795ef3785f34
Iwinosa Igbinosa <igbinosaiwinosa01@gmail.com>
2023-02-22 00:52:43 +0100

    push #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import { ReactComponent as Badge } from './image/Badge.svg'
+import { HiOutlineCheck } from 'react-icons/hi'
 
 function App() {
+  const totalBoards = 10
+  const [boardCount, setBoardCount] = useState(0)
+  const progressPercent = (boardCount / totalBoards) * 100
+  const handleClick = () => {
+    if (boardCount < totalBoards) {
+      setBoardCount((prevCount) => prevCount + 1)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='App'>
+      <article className='modal'>
+        <section className='badge'>
+          <article className='badge-border'>
+            <Badge />
+          </article>
+        </section>
+        <section className='text'>
+          <article className='modal-top-content'>
+            <h1>Upgrade to Riddey PRO and boost productivity!</h1>
+            <p>
+              Pellentesque porttitor euismod ante, accumsan consequat purus!
+            </p>
+          </article>
+          <article className='modal-features'>
+            <ul className='cards'>
+              <li className='card'>
+                <section className='check'>
+                  <HiOutlineCheck />
+                </section>
+                <section className='feature'>
+                  <p>
+                    <strong>Feature #1</strong> - Etiam convallis, nibh vitae
+                    dui risus
+                  </p>
+                </section>
+              </li>
+              <li className='card'>
+                <section className='check'>
+                  <HiOutlineCheck />
+                </section>
+                <section className='feature'>
+                  <p>
+                    <strong>Feature #2</strong> - Etiam convallis, nibh vitae
+                    dui risus
+                  </p>
+                </section>
+              </li>
+              <li className='card'>
+                <section className='check'>
+                  <HiOutlineCheck />
+                </section>
+                <section className='feature'>
+                  <p>
+                    <strong>Feature #3</strong> - Etiam convallis, nibh vitae
+                    dui risus
+                  </p>
+                </section>
+              </li>
+            </ul>
+            <section className='usage'>
+              <h2>Current usage</h2>
+              <h3>{boardCount} out of 10 boards in use</h3>
+              <section className='progress'>
+                <section
+                  className='progress-value'
+                  style={{ width: `${progressPercent}%` }}
+                ></section>
+              </section>
+            </section>
+          </article>
+          <button disabled={boardCount >= totalBoards} onClick={handleClick}>
+            Add New Board
+          </button>
+        </section>
+      </article>
+    </main>
+  )
 }
 
-export default App;
+export default App
